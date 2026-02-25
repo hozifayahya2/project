53d5279a9b29140b48473a981c7b70f420e196eb
Ahmed <akhames084@gmail.com>
2026-02-25 11:51:25 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+var cart = document.querySelector('.cart');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuBtn = document.getElementById('mobile-menu-btn');
+
+
+// عربه السلة 
+function open_close_cart() {
+    cart.classList.toggle("active");
+}
+
+
+
+
+//  زر المنيو 
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+
+
+
+
