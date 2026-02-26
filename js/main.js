d179aa18fa19ab0e156f250790110f5af0169c5c
hozifayahya2 <hozifayahya2@gmail.com>
2026-02-26 05:25:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,8 @@ function open_close_cart() {
 
 //button of sign-in page
   document.getElementById("loginBtn").onclick = function() {
-    window.location.href = "/Sign-in.html"
+    window.location.href = "./Sign-in.html"
   }
+
 
 
